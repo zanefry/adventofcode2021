64bb004fcfdadfbc35d0ee52d0c4efc76008c259
Zane Fry <zanefry@gmail.com>
2021-12-03 04:04:27 -0600

    3 part 2

---
+++ 3/part2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import sys
+
+def main():
+    bin_strings = []
+    with open(sys.argv[1]) as f:
+        bin_strings = [s.rstrip() for s in f.readlines()]
+
+    # most common filter
+    pool = bin_strings.copy()
+    for i in range(12):
+        num_ones = [s[i] for s in pool].count('1')
+        if num_ones >= len(pool) - num_ones:
+            most_common = '1'
+        else:
+            most_common = '0'
+
+        pool = [s for s in pool if s[i] == most_common]
+        if len(pool) == 1:
+            break
+    oxygen_generator_rating = int(pool[0], 2)
+
+    # least common filter
+    pool = bin_strings.copy()
+    for i in range(12):
+        num_ones = [s[i] for s in pool].count('1')
+        if num_ones < len(pool) - num_ones:
+            least_common = '1'
+        else:
+            least_common = '0'
+
+        pool = [s for s in pool if s[i] == least_common]
+        if len(pool) == 1:
+            break
+    co2_scrubber_rating = int(pool[0], 2)
+
+    print(f'{oxygen_generator_rating=} {co2_scrubber_rating=}')
+    print(oxygen_generator_rating * co2_scrubber_rating)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit('usage: ./part2 <inputfile>')
+    main()
